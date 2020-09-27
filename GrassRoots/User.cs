29daf936b9ecf122cb91f041be4031b1a5f2441f
Max_Gabriel_Susman <gabe.susman@gmail.com>
2020-09-27 10:11:37 -0600

    first commit

---
+++ GrassRoots/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace GrassRoots
+{
+    public class User
+    {
+        public string Username { get; set; }
+
+        public override string ToString()
+        {
+            return Username;
+        }
+    }
+}
